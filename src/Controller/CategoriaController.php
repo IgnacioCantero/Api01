50b8a100876cedccd2e126eb7a6b5a792cfcad9b
IgnacioCantero <ignacio.cantero@solvam.es>
2022-09-30 23:47:07 +0200

    Actualizado CategoriaController.php: añadida función getAllCategoriasAction

--- src/Controller/CategoriaController.php
+++ src/Controller/CategoriaController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Categorias;
+use App\Repository\CategoriasRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -17,30 +18,30 @@ class CategoriaController extends AbstractController
     //El Request es la petición de datos que hace el usuario
     //El EntityManagerInterface coge la info que va a llegar desde la app y la guarda en la bbdd, para ello hemos de pasar el objeto a modo entidad primero
     public function createCategoriaAction(Request $request, EntityManagerInterface $em) {
-        //1º -> Cogemos la información a guardar que nos viene en la petición (request)
+        //1º -> Coger la información a guardar que viene en la petición (request)
         $nombreCategoria = $request->get('categoria');
-        //2º -> Creo un objeto JsonResponse que va a ser la respuesta que enviaremos de vuelta
+        //2º -> Crear un objeto JsonResponse que va a ser la respuesta que se enviará de vuelta
         $response = new JsonResponse();
-        //3º -> Patrón de negación primero -> tengo que comprobar que la 'categoria' no venga a null o no venga (que no hayan errores)
+        //3º -> Patrón de negación primero -> comprobar que la 'categoria' no sea null o no exista (que no hayan errores)
         if (!$nombreCategoria) {
-            //Esto pasa si es null, si no tiene valor asignado, si es igual a 0 o si es false, es decir, nos han enviado mal los datos en la petición (request)
+            //Esto pasa si es null, si no tiene valor asignado, si es igual a 0 o si es false, es decir, han enviado mal los datos en la petición (request)
             $response->setData([
-                'succes' => false,
+                'success' => false,
                 'data' => null,
                 'error' => 'Categoria controller can´t be null or empty'
             ]);
             return $response;
         }
-        //4º -> Tengo que crear un nuevo objeto y setear sus atributos, ya que nos han enviado bien los datos de la petición (request)
+        //4º -> Crear un nuevo objeto y setear sus atributos, ya que han enviado bien los datos de la petición (request)
         $categoria = new Categorias();
         $categoria->setCategoria($nombreCategoria);
-        //5º -> Guardamos el objeto en bbdd con el EntityManagerInterface
+        //5º -> Guardar el objeto en bbdd con el EntityManagerInterface
         $em->persist($categoria);   //Doctrine -> persist() -> prepara la query para guardar el objeto en bbdd
         #$em->remove($categoria);   //Doctrine -> remove() -> prepara la query para borrar el objeto en bbdd
         $em->flush();   //Doctrine -> flush() -> ejecuta esa query
         //6º -> Devolver siempre una respuesta (response)
         $response->setData([
-            'succes' => true,
+            'success' => true,
             'data' => [
                 'id' => $categoria->getId(),
                 'nombre' => $categoria->getCategoria()
@@ -48,4 +49,43 @@ class CategoriaController extends AbstractController
         ]);
         return $response;
     }
+
+    /**
+     * @Route ("/categoria/list", name="list_categoria")
+     */
+    public function getAllCategoriasAction(CategoriasRepository $categoriasRepository) {
+        //1º -> Llamar al método repository que devuelve un Array
+        $categorias = $categoriasRepository->findAll();
+        //2º -> Crear un objeto JsonResponse que va a ser la respuesta que se enviará de vuelta
+        $response = new JsonResponse();
+        //3º -> Patrón de negación primero -> comprobar que la 'categoria' no sea null o no exista (que no hayan errores)
+        if (!$categorias) {
+            //Esto pasa si es null, si no tiene valor asignado, si es igual a 0 o si es false, es decir, han enviado mal los datos en la petición (request)
+            $response->setData([
+                'success' => false,
+                'data' => null,
+                'error' => 'Categoria controller can´t be null or empty'
+            ]);
+            return $response;
+        }
+        //4º -> Enviar Array '$categorias' en formato Json (como Symfony no sabe pasar de Array a Json, hay que pasar cada objeto del Array '$categorias' a Json por separado)
+        $categoriasAsArray = [];
+        foreach ($categorias as $cat) {
+            $categoriasAsArray[] = [
+                'id' => $cat->getId(),
+                'categoria' => $cat->getCategoria()
+            ];
+        }
+        //5º -> Devolver siempre una respuesta (response)
+        $response->setData([
+            'success' => true,
+            'data' => $categoriasAsArray
+        ]);
+        return $response;
+    }
 }
+
+
+
+
+
