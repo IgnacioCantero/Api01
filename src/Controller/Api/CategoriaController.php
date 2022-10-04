7dae95ec34b63df650c27b00d0887e010c849184
IgnacioCantero <ignacio.cantero@solvam.es>
2022-10-04 04:40:19 +0200

    -Añadido Categoria.yaml y api.yaml -Modificado fos_rest.yaml, services.yaml y framework.yaml -Actualizado CategoriaController.php

--- src/Controller/Api/CategoriaController.php
+++ src/Controller/Api/CategoriaController.php
@@ -2,7 +2,29 @@
 
 namespace App\Controller\Api;
 
-class CategoriaController
+use App\Repository\CategoriasRepository;
+use FOS\RestBundle\Controller\AbstractFOSRestController;
+use FOS\RestBundle\Controller\Annotations as Rest;
+
+/**
+ * @Rest\Route("/categoria")
+ */
+class CategoriaController extends AbstractFOSRestController
 {
+    private $categoriaRepository;
+
+    public function __construct(CategoriasRepository $repo)
+    {
+        $this->categoriaRepository = $repo;
+    }
+
+    //http://127.0.0.1:8000/api/categoria
+    /**
+     * @Rest\Get(path="/")
+     * @Rest\View(serializerGroups={"get_categorias"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function getCategorias() {
+        return $this->categoriaRepository->findAll();
+    }
 
 }
