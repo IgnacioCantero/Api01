41e6089ae4d585b35ed4bf8445a35ed1130b5f7d
IgnacioCantero <ignacio.cantero@solvam.es>
2022-10-07 18:38:22 +0200

    -Actualizado CategoriaController.php    ·GET->Añadida función getCategoriaAction    ·POST->Actualizada función createCategoriaAction    ·UPDATE->Añadida función updateCategoriaAction    ·DELETE->Añadida función deleteCategoriaAction -Añadido CategoriaType.php -Añadido Categorias.yaml -Modificado Categoria.yaml

--- src/Controller/Api/CategoriaController.php
+++ src/Controller/Api/CategoriaController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Api;
 
 use App\Entity\Categorias;
+use App\Form\CategoriaType;
 use App\Repository\CategoriasRepository;
 use FOS\RestBundle\Controller\AbstractFOSRestController;
 use FOS\RestBundle\Controller\Annotations as Rest;
@@ -15,6 +16,8 @@ use Symfony\Component\HttpFoundation\Response;
  */
 class CategoriaController extends AbstractFOSRestController
 {
+    //CRUD -> Create(1), Read(2), Update(3), Delete(4)
+
     private $categoriaRepository;
 
     public function __construct(CategoriasRepository $repo)
@@ -22,20 +25,38 @@ class CategoriaController extends AbstractFOSRestController
         $this->categoriaRepository = $repo;
     }
 
+    #2
     //http://127.0.0.1:8000/api/categoria/
     /**
      * @Rest\Get(path="/")
-     * @Rest\View(serializerGroups={"get_list_categorias"}, serializerEnableMaxDepthChecks=true)
+     * @Rest\View(serializerGroups={"get_list_all_categorias"}, serializerEnableMaxDepthChecks=true)
      */
     public function getCategoriasAction() {
         return $this->categoriaRepository->findAll();
     }
 
+    #2
+    //http://127.0.0.1:8000/api/categoria/1
+    /**
+     * @Rest\Get(path="/{id}")
+     * @Rest\View(serializerGroups={"get_list_one_categoria"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function getCategoriaAction(Request $request) {
+        $categoria = $this->categoriaRepository->find($request->get('id'));
+        if (!$categoria) {
+            return new JsonResponse("No se ha encontrado la categoría", Response::HTTP_NOT_FOUND);
+        }
+        return $categoria;
+    }
+
+/*
+    #1
+    //SIN FORMULARIOS
     //http://127.0.0.1:8000/api/categoria/
     /**
      * @Rest\Post(path="/")
      * @Rest\View(serializerGroups={"post_add_categoria"}, serializerEnableMaxDepthChecks=true)
-     */
+     * /
     public function createCategoriaAction(Request $request) {
         //1º -> Coger la información a guardar que viene en la petición (request)
         $categoria = $request->get('categoria');
@@ -47,9 +68,70 @@ class CategoriaController extends AbstractFOSRestController
         //4º -> Crear un nuevo objeto y setear sus atributos, ya que han enviado bien los datos de la petición (request)
         $cat = new Categorias();
         $cat->setCategoria($categoria);
-        //5º -> Guardar el objeto en bbdd con el EntityManagerInterface
+        //5º -> Guardar el objeto en bbdd
         $this->categoriaRepository->add($cat, true);
-        //6º -> Devolver siempre una respuesta (response)
+        //6º -> Devolver siempre una respuesta
         return $cat;
+    }
+*/
+
+    #1
+    //CON FORMULARIOS
+    //Sirven para manejar las peticiones y validar el tipado (validator) -> null/not null, texto en blanco, tamaño máximo de caracteres, etc
+    //http://127.0.0.1:8000/api/categoria/
+    /**
+     * @Rest\Post(path="/")
+     * @Rest\View(serializerGroups={"post_add_one_categoria"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function createCategoriaAction(Request $request) {
+        //1º -> Crear objeto vacío
+        $cat = new Categorias();
+        //2º -> Inicializar el form
+        $form = $this->createForm(CategoriaType::class, $cat);
+        //3º -> Indicar al formulario que maneje la request
+        $form->handleRequest($request);
+        //4º -> Comprobar si hay error (Patrón de negación primero)
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return $form;
+        }
+        //5º -> Guardar el objeto en bbdd
+        print_r($form->getData());
+        $this->categoriaRepository->add($form->getData(), true);
+        //6º -> Devolver siempre una respuesta
+        return $form->getData();
+    }
+
+    #3
+    /**
+     * @Rest\Patch(path="/{id}")
+     * @Rest\View(serializerGroups={"patch_update_one_categoria"}, serializerEnableMaxDepthChecks=true)
+     */
+    public function updateCategoriaAction(Request $request) {
+        $catedoriaID = $request->get('id');
+        $catedoria = $this->categoriaRepository->find($catedoriaID);
+        if (!$catedoria) {
+            return new JsonResponse("No se ha encontrado la categoría", Response::HTTP_NOT_FOUND);
+        }
+        $form = $this->createForm(CategoriaType::class, $catedoria, ['method'=>$request->getMethod()]);
+        $form->handleRequest($request);
+        if (!$form->isSubmitted() || !$form->isValid()) {
+            return new JsonResponse("Bad data", 400);
+        }
+        $this->categoriaRepository->add($catedoria, true);
+        return $catedoria;
+    }
+
+    #4
+    /**
+     * @Rest\Delete(path="/{id}")
+     */
+    public function deleteCategoriaAction(Request $request) {
+        $categoriaId = $request->get('id');
+        $categoria = $this->categoriaRepository->find($categoriaId);
+        if (!$categoria) {
+            return new JsonResponse("No se ha encontrado la categoría", 400);
+        }
+        $this->categoriaRepository->remove($categoria, true);
+        return new JsonResponse("Categoría borrada", 200);
     }
 }
